5f4038f103d3f88dab089b3fd987b7a956db24e8
benjaminbowen00 <benjibowen@hotmail.com>
2017-10-31 16:41:44 +0000

    functions written up to month name

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -13,3 +13,48 @@ end
 def multiply(num_1, num_2)
   return num_1 * num_2
 end
+
+def divide(num_1, num_2)
+  return num_1 / num_2
+end
+
+def length_of_string(string_1)
+  return string_1.length
+end
+
+def join_string(string_1, string_2)
+  return string_1 + string_2
+end
+
+def add_string_as_number(string_1, string_2)
+  return string_1.to_i + string_2.to_i
+end
+
+def number_to_full_month_name(num_1)
+   case num_1
+   when 1
+     return "January"
+   when 2
+     return "February"
+   when 3
+     return "March"
+   when 4
+     return "April"
+   when 5
+     return "May"
+   when 6
+     return "June"
+   when 7
+     return "July"
+   when 8
+     return "August"
+   when 9
+     return "September"
+   when 10
+     return "October"
+   when 11
+     return "November"
+   when 12
+     return "December"
+   end
+end
